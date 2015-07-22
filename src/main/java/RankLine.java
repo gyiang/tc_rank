9c1a6ba18dd989013938b4d875e9b53e6e2165ba
Gan Yi.ang <getbox@126.com>
2015-07-22 14:04:42 +0800

    added rank-0722

--- src/main/java/RankLine.java
+++ src/main/java/RankLine.java
@@ -13,9 +13,9 @@ import java.util.List;
  */
 public class RankLine {
 
-    Site site=Site.me().setRetryTimes(2).setTimeOut(20000).setSleepTime(5000).setDomain("tianchi.com")
-    .setUserAgent("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
-    File csv =new File("./rank/rank_"+DateFormat.getDateInstance().format(new Date())+".csv");
+    Site site = Site.me().setRetryTimes(2).setTimeOut(20000).setSleepTime(5000).setDomain("tianchi.com")
+            .setUserAgent("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+    File csv = new File("./rank/rank_" + DateFormat.getDateInstance().format(new Date()) + ".csv");
 
     public void pholcus(List<String> urls) {
         Spider.create(new PageProcessor() {
@@ -23,22 +23,22 @@ public class RankLine {
                 List<String> rank = page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div/p/text()").all();
                 List<String> teamName = page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[2]/div/p/text()").all();
                 List<String> member = page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[2]/div/div/div/text()").all();
-                List<String> organize=page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[3]/div/p[2]/text()").all();
-                List<String> score=page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[4]/text()").all();
-                List<String> date=page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[5]/text()").all();
+                List<String> organize = page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[3]/div/p[2]/text()").all();
+                List<String> score = page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[4]/text()").all();
+                List<String> date = page.getHtml().xpath("html/body/div[2]/div[3]/div[2]/div/ul[3]/li/div[5]/text()").all();
 
                 try {
-                    BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csv,true),"GBK"));
+                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(csv, true), "GBK"));
                     //bw.write("rank,team,member,organizew,score,date");
                     //bw.newLine();
-                    for (int i = 0; i < rank.size() ;i++) {
-                        if(teamName.get(i).contains(",")){
-                            teamName.set(i,teamName.get(i).replace(",",""));
+                    for (int i = 0; i < rank.size(); i++) {
+                        if (teamName.get(i).contains(",")) {
+                            teamName.set(i, teamName.get(i).replace(",", ""));
                         }
-                        bw.write(rank.get(i)+"," +teamName.get(i)+"," +member.get(i)+","
-                                +organize.get(i)+","
-                                +score.get(i)+","
-                                +date.get(i));
+                        bw.write(rank.get(i) + "," + teamName.get(i) + "," + member.get(i) + ","
+                                + organize.get(i) + ","
+                                + score.get(i) + ","
+                                + date.get(i));
                         bw.newLine();
                     }
                     bw.close();
@@ -58,10 +58,10 @@ public class RankLine {
     }
 
     public static void main(String[] args) {
-        List<String> urls=new ArrayList<String>();
+        List<String> urls = new ArrayList<String>();
 
-        for (int i=1;i<=10;i++){
-            urls.add("http://tianchi.aliyun.com/competition/rankingList.htm?season=1&raceId=3&pageIndex="+i);
+        for (int i = 1; i <= 10; i++) {
+            urls.add("http://tianchi.aliyun.com/competition/rankingList.htm?season=1&raceId=3&pageIndex=" + i);
         }
         new RankLine().pholcus(urls);
     }
